fc11d9626f6d8d8df0e0b1589773ab129c342adf
Dima Hinev <dh@Dimas-MacBook-Air.local>
2026-06-10 15:22:38 +0300

    init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="font-recoleta text-3xl">Brand Marketing</h1>
+      <h1 className="font-rounds text-3xl">Brand Marketing</h1>
+    </div>
+  );
+}
